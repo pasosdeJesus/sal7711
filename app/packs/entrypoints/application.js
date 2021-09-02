f78717fd3c436d1d725136e3c4325420713a494a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-02 17:10:19 -0500

    Actualiza

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -21,8 +21,9 @@ require('@rails/ujs').start()   // Javascript no intrusivo segun rails
 require('turbolinks').start()   // Acelera carga de paginas
 
 import $ from "expose-loader?exposes=$,jQuery!jquery"
+
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
-import 'bootstrap'              // Maquetacion y elementos de diseño
+import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
